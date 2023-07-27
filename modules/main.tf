e2b9fd3648b32a5959182ae991d49a9a76fcef9c
jjsen <jjsen.git@gmail.com>
2023-07-28 00:26:10 +0200

    test

--- modules/main.tf
+++ modules/main.tf
@@ -2,7 +2,7 @@ resource "google_project" "project" {
   name            = var.project_name
   project_id      = var.project_id
   billing_account = var.billing_account
-  region          = var.region
+
   org_id          = var.org
   folder_id       = var.folder_id
 }
