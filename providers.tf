847d50c6e5381ba28f3670e4a5d981097eae8b8d
jjsen <87337261+jjsen@users.noreply.github.com>
2023-08-21 17:48:56 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,11 +2,9 @@ terraform {
     required_providers {
         google = {
             source = "hashicorp/google"
-            version = "~> 4.50.0"
         }
         google-beta = {
             source = "hashicorp/google-beta"
-            version = "~> 4.0.0"
         }        
     }
 }
